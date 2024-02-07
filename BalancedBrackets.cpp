3f9d1ed7e0a2bcd6a163dca684168b18b2b58d38
Rebun <rubengdopazo@gmail.com>
2024-02-07 16:56:33 +0000

    All tests passed

--- BalancedBrackets.cpp
+++ BalancedBrackets.cpp
@@ -13,63 +13,78 @@ string rtrim(const string &);
  * The function is expected to return a STRING.
  * The function accepts STRING s as parameter.
  */
-string isBalanced(string s) {
+string isBalancedAlt(string s)
+{
     char openTag = 'o';
     map<char, char> brackets{
-        {'[',openTag},
-        {'{',openTag},
-        {'(',openTag},
-        {']','['},
-        {'}','{'},
-        {')','('}
-    };
+        {'[', openTag},
+        {'{', openTag},
+        {'(', openTag},
+        {']', '['},
+        {'}', '{'},
+        {')', '('}};
     stack<char> bracketsStack;
-    for(char c : s) {
+    for (char c : s)
+    {
         auto bracket = brackets.find(c);
-        if(bracket == brackets.end()) return "NO"; // Unexpected character
-        if(bracket->second == openTag) bracketsStack.push(c); // Opening bracket
-        else { // Closing bracket
-            if(bracketsStack.top() != bracket->second) return "NO";
-            else bracketsStack.pop();
+        if (bracket == brackets.end())
+            return "NO"; // Unexpected character
+        if (bracket->second == openTag)
+            bracketsStack.push(c); // Opening bracket
+        else
+        { // Closing bracket
+            if (bracketsStack.top() != bracket->second)
+                return "NO";
+            else
+                bracketsStack.pop();
         }
     }
-    if(!bracketsStack.empty()) return "NO";
+    if (!bracketsStack.empty())
+        return "NO";
     return "YES";
 }
 
-string isBalancedDebug(string s) {
-    char openTag = 'o';
-    map<char, char> brackets{
-        {'[',openTag},
-        {'{',openTag},
-        {'(',openTag},
-        {']','['},
-        {'}','{'},
-        {')','('}
-    };
+string isBalanced(string s)
+{
     stack<char> bracketsStack;
-    for(char c : s) {
-        auto bracket = brackets.find(c);
-        if(bracket == brackets.end()) { // Unexpected character
-            cout << "Unexpected character: " << c << endl;
+    for (char c : s)
+    {
+        cout << "Next: " << c << endl;
+        try {        
+            switch (c)
+            {
+            case '[':
+            case '(':
+            case '{':
+                bracketsStack.push(c);
+                break;
+            case ']':
+                if (bracketsStack.empty() || bracketsStack.top() != '[')
+                    return "NO";
+                else
+                    bracketsStack.pop();
+                break;
+            case ')':
+                if (bracketsStack.empty() || bracketsStack.top() != '(')
+                    return "NO";
+                else
+                    bracketsStack.pop();
+                break;
+            case '}':
+                if (bracketsStack.empty() || bracketsStack.top() != '{')
+                    return "NO";
+                else
+                    bracketsStack.pop();
+                break;
+            default:
+                break;
+            }
+        } catch (const exception& e) {
             return "NO";
         }
-        if(bracket->second == openTag) { // Opening bracket
-            bracketsStack.push(c);
-            cout << "New opening bracket: " << c << endl;
-        } else { // Closing bracket
-            if(bracketsStack.top() != bracket->second) {
-                cout << "Wrong closing bracket: " << c << endl;
-                return "NO";
-            }
-            else {
-                cout << "Correct closing bracket: " << c << endl;
-                bracketsStack.pop();
-            }
-        }
     }
-    if(!bracketsStack.empty()) {
-        cout << "Bracket stack not empty" << endl;
+    if (!bracketsStack.empty())
+    {
         return "NO";
     }
     return "YES";
@@ -82,7 +97,8 @@ int main()
 
     int t = stoi(ltrim(rtrim(t_temp)));
 
-    for (int t_itr = 0; t_itr < t; t_itr++) {
+    for (int t_itr = 0; t_itr < t; t_itr++)
+    {
         string s;
         getline(cin, s);
 
@@ -94,24 +110,27 @@ int main()
     return 0;
 }
 
-string ltrim(const string &str) {
+string ltrim(const string &str)
+{
     string s(str);
 
     s.erase(
         s.begin(),
-        find_if(s.begin(), s.end(), [](unsigned char c) { return !ispunct(c); })
-    );
+        find_if(s.begin(), s.end(), [](unsigned char c)
+                { return !ispunct(c); }));
 
     return s;
 }
 
-string rtrim(const string &str) {
+string rtrim(const string &str)
+{
     string s(str);
 
     s.erase(
-        find_if(s.rbegin(), s.rend(), [](unsigned char c) { return !ispunct(c); }).base(),
-        s.end()
-    );
+        find_if(s.rbegin(), s.rend(), [](unsigned char c)
+                { return !ispunct(c); })
+            .base(),
+        s.end());
 
     return s;
 }
